1c914bc26a3bf1b6e321dda3b636e3949b4fe305
Vadim Tschernezki <vadim.t@me.com>
2016-08-14 12:57:32 +0200

    added rational and changed go_crazy.

--- test.py
+++ test.py
@@ -2,5 +2,18 @@ print "hello world"
 
 def function go_crazy():
 	print  'go crazy!'
+        r1 = rational(1,2)
+        r2 = rational(2,3)
+        print r1.add(r2)
 	return 0
 
+class rational:
+    def __init__(self, a, b):
+        self._a = a
+        self._b = b
+
+    def add(self, r):
+        return rational(self._a - r.a, self._b - b)
+
+
+go_crazy()
